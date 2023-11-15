7ababbf4785c07bcbf42647f8ebfec5ffcb6d99d
JuliBercian <julissamilena97@gmail.com>
2023-11-15 14:27:11 -0600

    clase y reto2

--- ejemploclase7nov/src/test/java/pages/GoogleHomePageFactory.java
+++ ejemploclase7nov/src/test/java/pages/GoogleHomePageFactory.java
@@ -2,26 +2,33 @@ package pages;
 
 import static org.junit.Assert.assertEquals;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class GoogleHomePage {
+public class GoogleHomePageFactory {
 
     WebDriver webDriver;
-    private By textBoxSearch = By.id("APjFqb");
-    private By btnSearch = By.name("btnK");
 
-    public GoogleHomePage(WebDriver webDriver){
+    @FindBy(name = "q")
+    WebElement textBoxSearch;
+
+    @FindBy(name = "btnK")
+    WebElement btnSearch;
+
+    public GoogleHomePageFactory(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
     }
 
     private void fillTextBoxSearch(String text){
-        webDriver.findElement(textBoxSearch).clear();
-        webDriver.findElement(textBoxSearch).sendKeys(text);
+        textBoxSearch.clear();
+        textBoxSearch.sendKeys(text);
     }
 
     private void clickBtnSearch(){
-        webDriver.findElement(btnSearch).click();
+        btnSearch.click();
     }
 
     public void searchGoogle(String txtToSearch){
@@ -36,4 +43,6 @@ public class GoogleHomePage {
             webDriver.getTitle()
             );
     }
+
+    
 }
---
+++ tiendaenlinea/src/test/java/pages/TiendaEnLineaPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import features.tiendaEnLineaPage;
+
+public class TiendaEnLineaPage {
+    
+    WebDriver webDriver;
+
+    private By userName = By.id("username");
+    private By passWord = By.id("password");
+
+    public TiendaEnLinea(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    private void llenarAuntenticacion(String usuario, String contra) {
+        userName.clear();
+        userName.sendKeys(usuario);
+        passWord.clear();
+        passWord.sendKeys(contra);
+    }
+
+}
